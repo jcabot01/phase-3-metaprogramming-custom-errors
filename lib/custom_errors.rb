744d5772dcf03313b900c2a5d30ffa2603aaf996
JonCabot <jfc0053@gmail.com>
2022-03-26 20:58:20 -0500

    commit tree 01f3836f2d2512971a92ac6356de8418d87bfb10

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -6,10 +6,24 @@ class Person
   end
 
   def get_married(person)
-    self.partner = person
-    person.partner = self
+    if person.is_a?(Person)
+      self.partner = person
+      person.partner = self
+    else
+      begin 
+        raise PartnerError #first we raise the error class
+      rescue PartnerError => error  #then we rescue the error by creating an instance of PartnerError; which returns (puts) error.message from below
+        puts error.message
+      end 
+    end 
   end
 
+  # create an error handling class, assuming the scope of Standard Error messages (a hierachy of message types)
+  class PartnerError < StandardError
+    def message
+      "you must give the get_married method an argument of an instance of the person class!"
+    end 
+  end
 end
 
 beyonce = Person.new("Beyonce")
